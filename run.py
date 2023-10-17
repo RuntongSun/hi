46d48d4c29f80499ac08d7a1191c4826c99016fd
TonySun <pearlgrow@outlook.com>
2023-10-18 05:51:52 +0800

    Update run.py

--- run.py
+++ run.py
@@ -3,12 +3,6 @@ import sys
 
 from wxcloudrun import app
 
-@app.route('/wechat', methods=['GET', 'POST'])
-def wechat_handler():
-    # ... 处理微信消息 ...
-    return 'ok', 200  # 简单返回'ok'作为响应，以确保路由设置正确
-
-
 # 启动Flask Web服务
 if __name__ == '__main__':
     app.run(host=sys.argv[1], port=sys.argv[2])
